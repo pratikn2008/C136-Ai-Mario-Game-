26f8c5a502449303a1bbf9ea5f2f44d9ea2b70b0
Pratik Nayak <104261131+pratikn2008@users.noreply.github.com>
2023-01-22 11:30:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,11 +6,12 @@ function preload() {
 
 function setup() {
 	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
 	instializeInSetup(mario);
 }
 
 function draw() {
-	game();
+	game()
 }
 
 
