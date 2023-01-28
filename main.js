6885dd6308f140a2a7f6c71d74f427f0703ff2ed
Pratik Nayak <104261131+pratikn2008@users.noreply.github.com>
2023-01-28 16:34:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
-function preload() {
-	world_start = loadSound("world_start.wav");
-	setSprites();
-	MarioAnimation();
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+function preload(){
+	img = loadImage("mario05.png")
 }
 
-function setup() {
-	canvas = createCanvas(1240,336);
-	canvas.parent('canvas');
-	instializeInSetup(mario);
+function setup(){
+	createCanvas(650, 400);
+	video = createCapture(VIDEO);
+	video.size(600, 300);
+
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
 }
 
-function draw() {
-	game()
+function draw(){
+	background("#D3D3D3");
+	if(noseX < 300){
+		marioX = marioX - 1;
+	}
+	if(noseX > 300){
+		marioX = marioX + 1;
+	}
+	if(noseY < 150){
+		marioX = marioX - 1;
+	}
+	image(img, marioX, marioY, 40, 70);
 }
 
+function modelLoaded(){
+	console.log('Model Loaded!');
+}
 
-
-
-
-
+function gotPoses(results){
+	if(results.length > 0){
+	    noseX = results[0].pose.nose.x;
+		noseY = results[0].pose.nose.y;
+		console.log("noseX = " + noseX + "noseY = " + noseY)
+	}
+}
