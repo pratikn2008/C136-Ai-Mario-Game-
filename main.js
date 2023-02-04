e6e6eb50b314c14a095e7161ab8ebaa5ebeff346
Pratik Nayak <104261131+pratikn2008@users.noreply.github.com>
2023-02-04 16:28:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,30 +5,24 @@ marioX = 325;
 marioY = 325;
 
 function preload(){
-	img = loadImage("mario05.png")
+	img = loadImage("mario05.png");
+	world_start = loadSound("world_start.wav");
+	setSprites();
+	MarioAnimation();
 }
 
 function setup(){
-	createCanvas(650, 400);
+	createCanvas(1240, 336);
 	video = createCapture(VIDEO);
-	video.size(600, 300);
+	video.size(800, 400);
+	video.parent('game_console');
 
 	poseNet = ml5.poseNet(video, modelLoaded);
 	poseNet.on('pose', gotPoses);
 }
 
 function draw(){
-	background("#D3D3D3");
-	if(noseX < 300){
-		marioX = marioX - 1;
-	}
-	if(noseX > 300){
-		marioX = marioX + 1;
-	}
-	if(noseY < 150){
-		marioX = marioX - 1;
-	}
-	image(img, marioX, marioY, 40, 70);
+	game()
 }
 
 function modelLoaded(){
@@ -37,8 +31,10 @@ function modelLoaded(){
 
 function gotPoses(results){
 	if(results.length > 0){
-	    noseX = results[0].pose.nose.x;
+		noseX = results[0].pose.nose.x;
 		noseY = results[0].pose.nose.y;
-		console.log("noseX = " + noseX + "noseY = " + noseY)
+		console.log("noseX = " + noseX + "noseY  = " + noseY);
+		console.log(results);
 	}
 }
+
